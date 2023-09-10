4e772412b595d335ff27d5145e0437ae07f68dee
vesso8 <skittles8@abv.bg>
2023-09-10 11:30:12 +0300

    Updated the github url

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     author='Veselin Hubavenov',
     author_email='vhubavenov@gmail.com',
     description='Movies Dataset Analyzer',
-    url='https://github.com/vesso8/Movies-Dataset-Analyzer.git',
+    url='https://github.com/vesso8/Movies-Dataset.git',
 )
